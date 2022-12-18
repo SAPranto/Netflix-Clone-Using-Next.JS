e8fd25e9549441a5c5d0eb753186e9759a5ca8d7
Shihab Ahmed Pranto <sa.pranto16@gmail.com>
2022-12-18 14:25:36 +0600

    Home page default

---
+++ components/banner.js
@@ -0,0 +1,26 @@
+const Banner = (props) => {
+    const { title, subTitle, imgUrl } = props;
+  
+    const handleOnPlay = () => {
+      console.log("handleOnPlay");
+    };
+    return (
+      <div>
+        <h3>{title}</h3>
+        <h3>{subTitle}</h3>
+        <button onClick={handleOnPlay}>Play</button>
+        <div
+          style={{
+            backgroundImage: `url(${imgUrl})`,
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            backgroundSize: "cover",
+            backgroundPosition: "50% 50%",
+          }}
+        ></div>
+      </div>
+    );
+  };
+  
+  export default Banner;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
+import Banner from "../components/banner";
+
 export default function Home() {
   return (
-    <>
     <div className={styles.container}>
       <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <h1>Netflix</h1>
-      </div>
-    </>
-  )}
+
+      <Banner
+        title="Clifford the red dog"
+        subTitle="a very cute dog"
+        imgUrl="../static/clifford.webp"
+      />
+    </div>
+  );
+}
