0de163bd1368394552c85e67f083016b58d43691
Shihab Ahmed Pranto <sa.pranto16@gmail.com>
2022-12-22 16:04:42 +0600

    login page created

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,18 @@ import Banner from "../components/banner";
 import NavBar from "../components/navbar";
 import Card from "../components/card";
 import SectionCards from "../components/section-cards";
-import { getVideos } from "../lib/videos";
+import { getVideos,getPopularVideos} from "../lib/videos";
+
 
 export async function getServerSideProps(context) {
   const disneyVideos = await getVideos("Hollywood Movie trailers");
   const productivityVideos = await getVideos("Latest Bollywood Songs");
   const travelVideos = await getVideos("Valorant Highlights");
+  const popularVideos = await getPopularVideos()
   // const popularVideos = await getVideos();
 
   return {
-    props: { disneyVideos, travelVideos, productivityVideos }, // will be passed to the page component as props
+    props: { disneyVideos, travelVideos, productivityVideos, popularVideos }, // will be passed to the page component as props
   };
 }
 
@@ -22,24 +24,30 @@ export default function Home({
   disneyVideos,
   travelVideos,
   productivityVideos,
+  popularVideos
 }) {
   return (
     <div className={styles.container}>
       <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <NavBar username="ankita@ank.com" />
-      <Banner
-        title="Clifford the red dog"
-        subTitle="a very cute dog"
-        imgUrl="/static/clifford.webp"
-      />
-      <div className={styles.sectionWrapper}>
-        <SectionCards title="Latest Movie Trailers" videos={disneyVideos} size="large" />
-        <SectionCards title="Latest Songs" videos={productivityVideos} size="large" />
-        <SectionCards title="Valorant Highlights" videos={travelVideos} size="large" />
-      </div>
-    </div>
-  );
+        </Head>
+
+<div className={styles.main}>
+  <NavBar username="ankita@ank.com" />
+  <Banner
+    title="Clifford the red dog"
+    subTitle="a very cute dog"
+    imgUrl="/static/clifford.webp"
+  />
+
+  <div className={styles.sectionWrapper}>
+    <SectionCards title="Disney" videos={disneyVideos} size="large" />
+    <SectionCards title="Travel" videos={travelVideos} size="large" />
+    <SectionCards title="Productivity" videos={productivityVideos} size="large"/>
+    <SectionCards title="Popular" videos={popularVideos} size="large" />
+  </div>
+</div>
+</div>
+);
 }
---
+++ pages/login.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "../styles/Login.module.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [userMsg, setUserMsg] = useState("");
+  const router = useRouter();
+  const handleOnChangeEmail = (e) => {
+    setUserMsg("");
+    console.log("event", e);
+    const email = e.target.value;
+    setEmail(email);
+  };
+
+  const handleLoginWithEmail = async (e) => {
+    console.log("hi button");
+    e.preventDefault();
+
+    if (email) {
+      if (email === "kulkarni.ankita09@gmail.com") {
+        // route to dashboard
+        router.push("/");
+      } else {
+        console.log("Something went wrong logging in");
+      }
+    } else {
+      // show user message
+      setUserMsg("Enter a valid email address");
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Netflix SignIn</title>
+      </Head>
+      <header className={styles.header}>
+        <div className={styles.headerWrapper}>
+          <Link className={styles.logoLink} href="/">
+            
+              <div className={styles.logoWrapper}>
+                <Image
+                  src="/static/netflix.svg"
+                  alt="Netflix logo"
+                  width={128}
+                  height={34}
+                />
+              </div>
+            
+          </Link>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <div className={styles.mainWrapper}>
+          <h1 className={styles.signinHeader}>Sign In</h1>
+          <input
+            type="text"
+            placeholder="Email address"
+            className={styles.emailInput}
+            onChange={handleOnChangeEmail}
+          />
+
+          <p className={styles.userMsg}>{userMsg}</p>
+          <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
+            Sign In
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+};
+export default Login;
